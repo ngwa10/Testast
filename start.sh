2d33753c55547b9b30187ff6f5799519df1f7954
ngwa10 <todaybafutboy@gmail.com>
2025-10-02 16:10:01 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 echo "[INFO] Starting virtual display (Xvfb)..."
 Xvfb :1 -screen 0 1280x800x24 -ac &
 
-# wait for Xvfb to be ready
 sleep 2
 
 export DISPLAY=:1
-echo "[INFO] DISPLAY set to :1"
+export XAUTHORITY=/home/dockuser/.Xauthority
+touch /home/dockuser/.Xauthority
 
-# run your debug script
+echo "[INFO] DISPLAY set to :1"
 echo "[INFO] Starting debug_core.py..."
 python3 debug_core.py
