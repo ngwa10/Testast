5f6e6d3007d23380a9efebe6d157013885ee7cf6
ngwa10 <todaybafutboy@gmail.com>
2025-10-07 00:14:19 +0100

    Update core.py

--- core.py
+++ core.py
@@ -14,7 +14,7 @@ from typing import Optional
 import pyautogui
 
 import shared  # 👈 shared singleton
-
+import win_loss  # 👈 win/loss detection module
 # ---------------------------
 # Configuration
 # ---------------------------
